d9aee6acd1d47c76d0c0e32ae215a7927d8ee6ff
Brandon Icardi <brandonicardi@gmail.com>
2025-08-31 22:42:13 -0300

    Modificacion del codigo  + Quite comentario que rompia la vista.

--- backend/alojamientos.php
+++ backend/alojamientos.php
@@ -21,21 +21,19 @@ $sql = "SELECT
 $res = $conn->query($sql);
 $alojamientos = [];
 
-// Aca tengo que procesar las imagenes secundarias para las publicaciones pero antes revisando el formato, puedo hacerlo en array o en una tabla aparte.
+if ($res) {
+  while ($row = $res->fetch_assoc()) {
+    $row['precio_noche'] = isset($row['precio_noche']) ? (float)$row['precio_noche'] : 0.0;
 
-// if ($res) {
-//   while ($row = $res->fetch_assoc()) {
-//     $row['precio_noche'] = isset($row['precio_noche']) ? (float)$row['precio_noche'] : 0.0;
+    if (!empty($row['imagenes_secundarias'])) {
+      $row['imagenes_secundarias'] = array_map('trim', explode(',', $row['imagenes_secundarias']));
+    } else {
+      $row['imagenes_secundarias'] = [];
+    }
 
-//     if (!empty($row['imagenes_secundarias'])) {
-//       $row['imagenes_secundarias'] = array_map('trim', explode(',', $row['imagenes_secundarias']));
-//     } else {
-//       $row['imagenes_secundarias'] = [];
-//     }
-
-//     $alojamientos[] = $row;
-//   }
-// }
+    $alojamientos[] = $row;
+  }
+}
 
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($alojamientos, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
